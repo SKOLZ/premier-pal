ef523c8609fe79115ef8d771c61e752b04329516
SKOLZ <gabriel.zanzotti@gmail.com>
2025-11-02 18:59:41 +0100

    refactor

--- src/commands/roleTaggingTest.ts
+++ src/commands/roleTaggingTest.ts
@@ -4,36 +4,17 @@ import { ActionRowBuilder, ComponentType, RoleSelectMenuBuilder, SlashCommandBui
 export default {
   data: new SlashCommandBuilder()
     .setName('role_tagging_test')
-    .setDescription('replies tagging the specified role to test if a notification is received!'),
+    .setDescription('replies tagging the specified role to test if a notification is received!')
+    .addRoleOption((option) =>
+      option
+        .setName('role')
+        .setDescription('The role to be tagged in the test message')
+        .setRequired(true),
+    ),
   execute: async (interaction: ChatInputCommandInteraction<CacheType>) => {
-    const roleSelect = new RoleSelectMenuBuilder()
-      .setCustomId('role_selection')
-      .setPlaceholder('Select a role to be tagged for tournament reminders')
-      .setMinValues(1)
-      .setMaxValues(1);
-
-    const roleRow =
-      new ActionRowBuilder<RoleSelectMenuBuilder>().addComponents(roleSelect);
-
-    const roleResponse = await interaction.reply({
-      content: 'Select a role to test tagging it:',
-      components: [roleRow],
-      withResponse: true,
-    });
-
-    const roleCollector =
-      roleResponse.resource?.message?.createMessageComponentCollector({
-        componentType: ComponentType.RoleSelect,
-        time: 300_000,
-      });
-
-    roleCollector?.on('collect', async (roleInteraction) => {
-      const selectedRole = roleInteraction.values[0];
-      await roleInteraction.reply({
-        content: `This is a test message tagging the selected role: <@&${selectedRole}>`,
-      });
-      // setTimeout(async () => {
-      // }, 2000);
+    const selectedRole = interaction.options.getRole('role');
+    await interaction.reply({
+      content: `This is a test message tagging the selected role: <@&${selectedRole}>`,
     });
   },
 };
